aa3420602533233679bab4bfba1212c78e34e2c9
Anton Adamansky <adamansky@gmail.com>
2022-08-14 18:50:15 +0700

    *

--- src/iwstart.c
+++ src/iwstart.c
@@ -1,17 +1,21 @@
 #include "iwstart.h"
 #include "config.h"
 
+#include <iowow/iwxstr.h>
 #include <iowow/iwlog.h>
 #include <iowow/iwp.h>
+#include <iowow/iwutils.h>
+#include <iowow/iwini.h>
 
 #include <errno.h>
+#include <getopt.h>
+#include <limits.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <getopt.h>
-#include <limits.h>
 
 struct env g_env = { 0 };
 
@@ -25,7 +29,7 @@ static void _usage(const char *err) {
     fprintf(stderr, "\t%s\n\n", err);
   }
   fprintf(stderr, "\n\tIOWOW/IWNET/EJDB2 Project boilerplate generaror\n");
-  fprintf(stderr, "\nUsage %s [options]\n", g_env.program_file);
+  fprintf(stderr, "\nUsage %s [options]\n", g_env.program);
   fprintf(stderr, "\t-c, --conf=<>\t\t.ini configuration file\n");
   fprintf(stderr, "\t-V, --verbose\t\tPrint verbose output\n");
   fprintf(stderr, "\t-v, --version\t\tShow program version\n");
@@ -40,6 +44,56 @@ static void _on_signal(int signo) {
   // iwn_poller_shutdown_request(g_env.poller);
   exit(0);
 }
+
+static int _ini_handler(void *user_data, const char *section, const char *name, const char *value) {
+  iwrc rc = 0;
+  struct init *init = user_data;
+  iwlog_info("%s:%s=%s", section, name, value);
+  if (!section || !strcmp(section, "main")) {
+    if (!strcmp(name, "verbose")) {
+      IWINI_PARSE_BOOL(g_env.verbose);
+    }
+  }
+  return rc == 0;
+}
+
+static const char* _replace_config_env(const char *key, void *op) {
+  if (!strcmp(key, "{home}")) {
+    return getenv("HOME");
+  } else if (!strcmp(key, "{cwd}")) {
+    return g_env.cwd;
+  }
+  return 0;
+}
+
+static iwrc _config_load(void) {
+  iwrc rc = 0;
+  IWXSTR *xstr = 0;
+  char *data = 0;
+  size_t len;
+
+  RCB(finish, g_env.config_file_dir = iwpool_strdup2(g_env.pool, g_env.config_file));
+  g_env.config_file_dir = iwp_dirname((char*) g_env.config_file_dir);
+
+  data = iwu_file_read_as_buf_len(g_env.config_file, &len);
+  if (!data) {
+    rc = IW_ERROR_FAIL;
+    iwlog_error("Error reading configuration file: %s", g_env.config_file);
+    goto finish;
+  }
+
+  static const char *config_keys[] = { "{home}", "{cwd}" };
+  RCC(rc, finish, iwu_replace(&xstr, data, len,
+                              config_keys, sizeof(config_keys) / sizeof(config_keys[0]),
+                              _replace_config_env, 0));
+
+  rc = iwini_parse_string(iwxstr_ptr(xstr), _ini_handler, 0);
+
+finish:
+  return rc;
+}
+
+static iwrc _run(void);
 
 static int _main(int argc, char *argv[]) {
   int rv = EXIT_SUCCESS;
@@ -58,17 +112,29 @@ static int _main(int argc, char *argv[]) {
   RCC(rc, finish, iw_init());
   RCB(finish, g_env.pool = iwpool_create_empty());
 
-  char *buf = getcwd(0, 0);
-  if (!buf) {
-    rc = iwrc_set_errno(IW_ERROR_ERRNO, errno);
-    goto finish;
+  {
+    char *buf = getcwd(0, 0);
+    if (!buf) {
+      rc = iwrc_set_errno(IW_ERROR_ERRNO, errno);
+      goto finish;
+    }
+
+    g_env.cwd = iwpool_strdup2(g_env.pool, buf), free(buf);
+    RCB(finish, g_env.cwd);
   }
 
-  g_env.cwd = iwpool_strdup2(g_env.pool, buf), free(buf);
-  RCB(finish, g_env.cwd);
+  {
+    char path[PATH_MAX];
+    RCC(rc, finish, iwp_exec_path(path, sizeof(path)));
+    RCB(finish, g_env.program_file = iwpool_strdup2(g_env.pool, path));
+    g_env.program = argc ? argv[0] : "";
+  }
 
-  char path[PATH_MAX];
-  RCC(rc, finish, iwp_exec_path(path, sizeof(path)));
+  g_env.argc = argc;
+  RCB(finish, g_env.argv = iwpool_alloc(argc * sizeof(char*), g_env.pool));
+  for (int i = 0; i < argc; ++i) {
+    RCB(finish, g_env.argv[i] = iwpool_strdup2(g_env.pool, argv[i]));
+  }
 
   static const struct option long_options[] = {
     { "help",    0, 0, 'h' },
@@ -99,6 +165,12 @@ static int _main(int argc, char *argv[]) {
     }
   }
 
+  if (g_env.config_file) {
+    RCC(rc, finish, _config_load());
+  }
+
+  rc = _run();
+
 finish:
   _destroy();
   if (rc) {
@@ -107,13 +179,16 @@ finish:
   return rv;
 }
 
+static iwrc _run(void) {
+  iwrc rc = 0;
+  fprintf(stderr, "Hello\n");
+  return rc;
+}
+
 #ifdef IW_EXEC
 
 int main(int argc, char *argv[]) {
-  const char *v = VERSION_FULL;
-  fprintf(stderr, "%s\n", v);
-  //return _main(argc, argv);
-  return 0;
+  return _main(argc, argv);
 }
 
 #endif
--- src/iwstart.h
+++ src/iwstart.h
@@ -5,6 +5,7 @@
 
 struct env {
   const char  *cwd;
+  const char  *program;
   const char  *program_file;
   const char  *config_file;
   const char  *config_file_dir;
