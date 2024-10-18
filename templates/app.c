4d04215a8842ff4e84c5b4a628d4cef7eab6d46a
Anton Adamansky <adamansky@gmail.com>
2024-10-18 22:04:44 +0700

    * Generated app features improvements

--- templates/app.c
+++ templates/app.c
@@ -6,6 +6,7 @@
 #include <iowow/iwp.h>
 #include <iowow/iwutils.h>
 #include <iowow/iwini.h>
+#include <iowow/iwarr.h>
 
 #include <errno.h>
 #include <getopt.h>
@@ -15,13 +16,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/prctl.h>
 #include <unistd.h>
+#include <pthread.h>
 
 struct env g_env = { 0 };
 
-static void _destroy(void) {
-  iwpool_destroy(g_env.pool);
-}
+static pthread_mutex_t _mtx = PTHREAD_MUTEX_INITIALIZER;
+static pthread_barrier_t _barrier_term_signal_worker;
+static pthread_t _thr_term_signal_worker = -1;
+static struct iwulist _shutdown_hooks = { .usize = sizeof(void (*)(void)) };
+
+/// Threads registry
+static struct _thread {
+  pthread_t t;
+  bool      taken;
+  struct _thread *next;
+} *_threads;
 
 static void _usage(const char *err) {
   if (err) {
@@ -30,18 +41,161 @@ static void _usage(const char *err) {
   }
   fprintf(stderr, "\n\t@project_name@\n");
   fprintf(stderr, "\nUsage %s [options]\n", g_env.program);
-  fprintf(stderr, "\t-c, --conf=<>\t\t.ini configuration file\n");
-  fprintf(stderr, "\t-V, --verbose\t\tPrint verbose output\n");
-  fprintf(stderr, "\t-v, --version\t\tShow program version\n");
+  fprintf(stderr, "\t-c, --conf=<>          .ini configuration file\n");
+  fprintf(stderr, "\t-V, --verbose          Print verbose output\n");
+  fprintf(stderr, "\t-v, --version          Show program version\n");
+  fprintf(stderr, "\t    --breakout-sigint  Enable gdb breaks on SIGINT.\n");
   fprintf(stderr, "\t-h, --help\t\tPrint usage help\n");
   fprintf(stderr, "\n");
 };
+
+void app_shutdown_request(void) {
+  iwlog_warn2("@project_artifact@:: Shutdown requested");
+  if (_thr_term_signal_worker != -1) {
+    pthread_kill(_thr_term_signal_worker, SIGTERM);
+  } else {
+    exit(0);
+  }
+}
+
+void app_shutdown_hook_register(void (*hook)(void)) {
+  pthread_mutex_lock(&_mtx);
+  iwulist_push(&_shutdown_hooks, &hook);
+  pthread_mutex_unlock(&_mtx);
+}
+
+iwrc app_thread_register(pthread_t t) {
+  iwrc rc = 0;
+  struct _thread *node;
+  RCB(finish, node = malloc(sizeof(*node)));
+  pthread_mutex_lock(&_mtx);
+  node->t = t;
+  node->taken = false;
+  node->next = _threads;
+  _threads = node;
+  pthread_mutex_unlock(&_mtx);
+finish:
+  return rc;
+}
+
+void app_thread_unregister_join(pthread_t t) {
+  struct _thread *node = 0;
+  bool take = false;
+  pthread_mutex_lock(&_mtx);
+  for (struct _thread *n = _threads, *p = 0; n; p = n, n = n->next) {
+    if (n->t == t) {
+      node = n;
+      if (p) {
+        p->next = n->next;
+      } else {
+        _threads = 0;
+      }
+      if (!node->taken) {
+        node->taken = true;
+        take = true;
+      }
+      break;
+    }
+  }
+  pthread_mutex_unlock(&_mtx);
+  if (take) {
+    pthread_join(t, 0);
+  }
+  free(node);
+}
+
+void app_thread_unregister_detach(pthread_t t) {
+  struct _thread *node = 0;
+  bool take = false;
+  pthread_mutex_lock(&_mtx);
+  for (struct _thread *n = _threads, *p = 0; n; p = n, n = n->next) {
+    if (n->t == t) {
+      node = n;
+      if (p) {
+        p->next = n->next;
+      } else {
+        _threads = 0;
+      }
+      if (!node->taken) {
+        node->taken = true;
+        take = true;
+      }
+      break;
+    }
+  }
+  pthread_mutex_unlock(&_mtx);
+  if (take) {
+    pthread_detach(t);
+  }
+  free(node);
+}
+
+struct _thread_run_spec {
+  void (*worker)(void*);
+  struct iwref_holder *ref;
+  void *user_data;
+};
+
+static void* _thread_run(void *d) {
+  struct _thread_run_spec *spec = d;
+  spec->worker(spec->user_data);
+  if (spec->ref) {
+    iwref_unref(spec->ref);
+  }
+  free(spec);
+  return 0;
+}
+
+iwrc app_thread_run_ref(struct iwref_holder *ref, void (*worker)(void*), void *user_data) {
+  iwrc rc = 0;
+  pthread_t th;
+
+  struct _thread_run_spec *spec = malloc(sizeof(*spec));
+  RCRA(spec);
+  if (ref) {
+    iwref_ref(ref);
+  }
+  spec->worker = worker;
+  spec->ref = ref;
+  spec->user_data = user_data;
+
+  int rci = pthread_create(&th, 0, _thread_run, spec);
+  if (rci) {
+    rc = iwrc_set_errno(IW_ERROR_THREADING_ERRNO, rci);
+    goto finish;
+  }
+
+  app_thread_register(th);
+
+finish:
+  if (rc) {
+    if (ref) {
+      iwref_unref(ref);
+    }
+    free(spec);
+  }
+  return rc;
+}
+
+iwrc app_thread_run(void (*worker)(void*), void *user_data) {
+  return app_thread_run_ref(0, worker, user_data);
+}
+
+static void _shutdown_hooks_run(void) {
+  pthread_mutex_lock(&_mtx);
+  for (int i = 0, l = iwulist_length(&_shutdown_hooks); i < l; ++i) {
+    void (*hook)(void) = *(void(**)(void))iwulist_get(&_shutdown_hooks, i);
+    hook();
+  }
+  iwulist_destroy_keep(&_shutdown_hooks);
+  pthread_mutex_unlock(&_mtx);
+}
 
 static void _on_signal(int signo) {
   if (g_env.verbose) {
     fprintf(stderr, "\nExiting on signal: %d\n", signo);
   }
-  // iwn_poller_shutdown_request(g_env.poller);
+  // CHECK: iwn_poller_shutdown_request(g_env.poller);
   exit(0);
 }
 
@@ -67,7 +221,7 @@ static const char* _replace_config_env(const char *key, void *op) {
 
 static iwrc _config_load(void) {
   iwrc rc = 0;
-  IWXSTR *xstr = 0;
+  struct iwxstr *xstr = 0;
   char *data = 0;
   size_t len;
 
@@ -94,24 +248,153 @@ finish:
   return rc;
 }
 
+static void _shutdown(void) {
+  static bool shutdown = false;
+  if (__sync_bool_compare_and_swap(&shutdown, false, true)) {
+    if (_thr_term_signal_worker != -1) {
+      // Wait termination of signal handler thread
+      pthread_kill(_thr_term_signal_worker, SIGUSR1);
+      pthread_join(_thr_term_signal_worker, 0);
+      _thr_term_signal_worker = -1;
+    }
+
+    _shutdown_hooks_run();
+
+    {
+      /// Join all registered threads
+      int tn = 0;
+      pthread_mutex_lock(&_mtx);
+      for (struct _thread *n = _threads; n; n = n->next) {
+        ++tn;
+      }
+
+      pthread_t jt[tn];
+      tn = 0;
+      for (struct _thread *n = _threads; n; n = n->next) {
+        if (!n->taken) {
+          n->taken = true;
+          jt[tn++] = n->t;
+        }
+      }
+      pthread_mutex_unlock(&_mtx);
+
+      for (int i = 0; i < tn; ++i) {
+        pthread_join(jt[i], 0);
+      }
+    }
+
+    //CHECK:
+    //iwn_poller_destroy(&g_env.poller);
+  }
+}
+
+static void _deinit(void) {
+  _shutdown();
+  for (struct _thread *t = _threads, *n = 0; t; t = n) {
+    n = t->next;
+    free(t);
+  }
+  pthread_barrier_destroy(&_barrier_term_signal_worker);
+  pthread_mutex_destroy(&_mtx);
+  iwpool_destroy(g_env.pool);
+}
+
+IW_CONSTRUCTOR static void _init(void) {
+  static bool init = false;
+  if (__sync_bool_compare_and_swap(&init, false, true)) {
+    umask(0077);
+    if (!iw_init()) {
+      abort();
+    }
+    g_env.pool = iwpool_create_empty();
+    if (!g_env.pool) {
+      abort();
+    }
+    if (pthread_barrier_init(&_barrier_term_signal_worker, 0, 2)) {
+      abort();
+    }
+    atexit(_deinit);
+  }
+}
+
+static void _breakout_sigint(void) {
+  // In order to stop on Ctrl-C in GDB add breakpoint on line below
+  // then run app with --breakout-sigint flag
+  fprintf(stderr, "Breakout sigint\n");
+}
+
+static void _on_term_signal(int signo) {
+  if (g_env.verbose) {
+    fprintf(stderr, "\nExiting on signal: %d\n", signo);
+  }
+  _shutdown_hooks_run();
+  // TODO: Call app termination staff like event loop exit, etc.
+  // iwn_poller_shutdown_request(g_env.poller);
+}
+
+static void* _on_term_signal_worker(void *d) {
+  int sig;
+  sigset_t *ss = d;
+  iwp_set_current_thread_name("@project_artifact@");
+  pthread_barrier_wait(&_barrier_term_signal_worker);
+
+retry:
+  sig = 0, sigwait(ss, &sig);
+  switch (sig) {
+    case SIGINT:
+      if (g_env.breakout_sigint) {
+        _breakout_sigint();
+        goto retry;
+      }
+    case SIGTERM:
+      _on_term_signal(sig);
+    case SIGUSR1:
+      break;
+    default:
+      goto retry;
+  }
+  return 0;
+}
+
 iwrc run(void);
 
 static int _main(int argc, char *argv[]) {
   int rv = EXIT_SUCCESS;
   iwrc rc = 0;
-  umask(0077);
-  signal(SIGPIPE, SIG_IGN);
-  signal(SIGHUP, SIG_IGN);
-  signal(SIGALRM, SIG_IGN);
-  signal(SIGUSR1, SIG_IGN);
-  signal(SIGUSR2, SIG_IGN);
+  sigset_t ss;
 
-  if (  signal(SIGTERM, _on_signal) == SIG_ERR
-     || signal(SIGINT, _on_signal) == SIG_ERR) {
-    return EXIT_FAILURE;
+  if (prctl(PR_SET_PDEATHSIG, SIGKILL) == -1) {
+    perror("Failed to set: prctl(PR_SET_PDEATHSIG, SIGKILL)");
+    return -1;
   }
-  RCC(rc, finish, iw_init());
-  RCB(finish, g_env.pool = iwpool_create_empty());
+
+#ifndef NDEBUG
+  if (prctl(PR_SET_PTRACER, PR_SET_PTRACER_ANY) == -1) {
+    perror("Failed to se: prctl(PR_SET_PTRACER, PR_SET_PTRACER_ANY)");
+    return -1;
+  }
+#endif
+
+  _init();
+
+  {
+    sigemptyset(&ss);
+    sigaddset(&ss, SIGTERM);
+    sigaddset(&ss, SIGINT);
+    sigaddset(&ss, SIGUSR1);
+    sigaddset(&ss, SIGUSR2);
+    sigaddset(&ss, SIGHUP);
+    sigaddset(&ss, SIGALRM);
+    sigaddset(&ss, SIGPIPE);
+    pthread_sigmask(SIG_BLOCK, &ss, NULL);
+
+    int rci = pthread_create(&_thr_term_signal_worker, 0, _on_term_signal_worker, &ss);
+    if (rci) {
+      iwlog_ecode_error2(iwrc_set_errno(IW_ERROR_THREADING_ERRNO, rci), "Error creating signal handling thread");
+      return EXIT_FAILURE;
+    }
+    pthread_barrier_wait(&_barrier_term_signal_worker);
+  }
 
   {
     char *buf = getcwd(0, 0);
@@ -119,13 +402,12 @@ static int _main(int argc, char *argv[]) {
       rc = iwrc_set_errno(IW_ERROR_ERRNO, errno);
       goto finish;
     }
-
     g_env.cwd = iwpool_strdup2(g_env.pool, buf), free(buf);
     RCB(finish, g_env.cwd);
   }
 
   {
-    char path[PATH_MAX];
+    char path[PATH_MAX + 1];
     RCC(rc, finish, iwp_exec_path(path, sizeof(path)));
     RCB(finish, g_env.program_file = iwpool_strdup2(g_env.pool, path));
     g_env.program = argc ? argv[0] : "";
@@ -138,10 +420,11 @@ static int _main(int argc, char *argv[]) {
   }
 
   static const struct option long_options[] = {
-    { "help",    0, 0, 'h' },
+    { "help", 0, 0, 'h' },
     { "verbose", 0, 0, 'V' },
     { "version", 0, 0, 'v' },
-    { "conf",    1, 0, 'c' },
+    { "conf", 1, 0, 'c' },
+    { "breakout-sigint", 0, 0, -2 },
     0
   };
 
@@ -160,6 +443,9 @@ static int _main(int argc, char *argv[]) {
       case 'c':
         g_env.config_file = iwpool_strdup2(g_env.pool, optarg);
         break;
+      case -2:
+        g_env.breakout_sigint = true;
+        break;
       default:
         _usage(0);
         rv = EXIT_FAILURE;
@@ -174,7 +460,7 @@ static int _main(int argc, char *argv[]) {
   rc = run();
 
 finish:
-  _destroy();
+  _shutdown();
   if (rc) {
     return EXIT_FAILURE;
   }
--- templates/app.h
+++ templates/app.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <iowow/iwpool.h>
+#include <iowow/iwrefs.h>
+#include <pthread.h>
 
 struct env {
   const char  *cwd;
@@ -9,9 +11,24 @@ struct env {
   const char  *config_file;
   const char  *config_file_dir;
   const char **argv;
-  IWPOOL      *pool;
+  struct iwpool      *pool;
   int  argc;
   bool verbose;
+  volatile bool breakout_sigint;
 };
 
 extern struct env g_env;
+
+void app_shutdown_request(void);
+
+void app_shutdown_hook_register(void (*)(void));
+
+iwrc app_thread_register(pthread_t);
+
+void app_thread_unregister_join(pthread_t);
+
+void app_thread_unregister_detach(pthread_t);
+
+iwrc app_thread_run_ref(struct iwref_holder *ref, void (*)(void*), void* user_data);
+
+iwrc app_thread_run(void (*)(void*), void* user_data);
