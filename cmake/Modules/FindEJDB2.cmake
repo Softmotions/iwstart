806ce56a20e321172457b659773308e995e444ac
Anton Adamansky <adamansky@gmail.com>
2025-03-02 19:34:31 +0700

    * Fixed iowow,iwnet,ejdb2 CMake find scripts

--- cmake/Modules/FindEJDB2.cmake
+++ cmake/Modules/FindEJDB2.cmake
@@ -2,7 +2,7 @@ mark_as_advanced(EJDB2_INCLUDE_DIRS EJDB2_STATIC_LIB)
 
 find_path(EJDB2_INCLUDE_DIRS NAMES ejdb2/ejdb2.h)
 
-find_library(EJDB2_STATIC_LIB NAMES libejdb2-1.a)
+find_library(EJDB2_STATIC_LIB NAMES libejdb2-2.a)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(EJDB2 DEFAULT_MSG EJDB2_INCLUDE_DIRS
@@ -13,5 +13,6 @@ if(EJDB2_FOUND)
     EJDB2::static
     PROPERTIES IMPORTED_LINK_INTERFACE_LANGUAGES "C"
                IMPORTED_LOCATION ${EJDB2_STATIC_LIB}
-               IMPORTED_LINK_INTERFACE_LIBRARIES "IWNET::static")
+               IMPORTED_LINK_INTERFACE_LIBRARIES "IWNET::static"
+               INTERFACE_INCLUDE_DIRECTORIES ${EJDB2_INCLUDE_DIRS})
 endif()
